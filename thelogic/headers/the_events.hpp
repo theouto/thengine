a0303e193373ab398d659dbb63094a748397471f
theouto <tomas@otero.pro>
2026-07-10 16:57:45 +0200

    woops

--- thelogic/headers/the_events.hpp
+++ thelogic/headers/the_events.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "the_window.hpp"
-#include "../../thirdparty/imgui/imgui_impl_sdl3.cpp"
+#include "../../thirdparty/imgui/imgui_impl_sdl3.h"
 
 //this may be the most "omaga, OOP!!!!" code that I've ever made
 namespace the
